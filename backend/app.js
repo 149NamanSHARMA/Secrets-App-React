00ff386c597ad0db7095c408a3bd93e184c78cfa
149NamanSHARMA <103133304+149NamanSHARMA@users.noreply.github.com>
2024-07-24 11:11:57 +0530

    NODE_ENV added

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
-  cookie: { secure: false, httpOnly: true, sameSite: 'Lax' } 
+  cookie: { secure: process.env.NODE_ENV === 'production', httpOnly: true, sameSite: 'Lax' } 
 }));
 
 // Passport middleware
@@ -44,4 +44,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB Connected')).catch(err => console.log(err));
-
